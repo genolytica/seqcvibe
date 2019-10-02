ef0302dc20b69ac62610d3c3fa83cd09874aa136
Makis Digaletos <makisdig@gmail.com>
2019-10-02 18:00:29 +0300

    Reverted mm10 expression viewer hack. Initiated manual - Step_15

--- server/expressionViewerTab/expressionViewerItemKnownGene.R
+++ server/expressionViewerTab/expressionViewerItemKnownGene.R
@@ -97,23 +97,18 @@ expressionExplorerTabPanelRenderUI <- function(output,session,
                 samples <- as.character(M[which(as.character(M$class)==x),
                     "sample_id"])
 
-                # HACK - indexing D (counts table) to exclude gene names not present in dbGene (rda)
-                # This issue appears onlu in mm10.rda
-                ind <- rownames(D$norm) %in% dbGene$gene_id
-                ind_no <- grep('TRUE', ind)
-
                 switch(input$rnaExpressionGeneList,
                     all = {
                         switch(input$rnaExpressionMeasureRadio,
                             raw = {
-                                tab <- D$counts[ind_no,samples,drop=FALSE]
+                                tab <- D$counts[,samples,drop=FALSE]
                                 average <- round(apply(tab,1,
                                     input$rnaExpressionAverageRadio))
                                 deviation <- round(apply(tab,1,
                                     input$rnaExpressionDeviationRadio))
                             },
                             norm = {
-                                tab <- D$norm[ind_no,samples,drop=FALSE]
+                                tab <- D$norm[,samples,drop=FALSE]
                                 average <- round(apply(tab,1,
                                     input$rnaExpressionAverageRadio))
                                 deviation <- round(apply(tab,1,
@@ -121,7 +116,7 @@ expressionExplorerTabPanelRenderUI <- function(output,session,
                             },
                             rpkm = {
                                 tab <- round(edgeR::rpkm(
-                                    D$counts[ind_no,samples,drop=FALSE],
+                                    D$counts[,samples,drop=FALSE],
                                     gene.length=D$length,
                                     lib.size=unlist(D$libsize[samples]),
                                     ),digits=6)
@@ -131,7 +126,7 @@ expressionExplorerTabPanelRenderUI <- function(output,session,
                                     input$rnaExpressionDeviationRadio),digits=6)
                             },
                             rpgm = {
-                                tab <- round(D$norm[ind_no,samples,
+                                tab <- round(D$norm[,samples,
                                     drop=FALSE]/D$length,digits=6)
                                 average <- round(apply(tab,1,
                                     input$rnaExpressionAverageRadio),digits=6)
--- ui/helpTab/helpItemDoc.R
+++ ui/helpTab/helpItemDoc.R
@@ -1,5 +1,9 @@
 docTabPanel <- function() {
     fluidRow(column(12,
-        h4("Coming soon!")
+        tags$iframe(
+            src="manual.html",
+            width="100%",height=860,
+            frameborder=0
+        )
     ))
 }
