993f352f35b8fd80921f9b740455c55e5bc7b822
Makis Digaletos <makisdig@gmail.com>
2019-11-05 13:40:13 +0200

    Fixed session_loader messages issue

--- config/metadata.R
+++ config/metadata.R
@@ -21,22 +21,14 @@ metadata$quality <- 0
 
 
 # FUNCTION 1 apply to all norm_factor files and merge into 1 DF
-<<<<<<< HEAD
 IDs <- as.list(list.files(recursive = F, pattern = "PRJEB|GSE"))
-=======
-IDs <- as.list(list.files(recursive = F, pattern = "PRJEB|GSE|ASCL2"))
->>>>>>> 88a810c224c4f51971cacb88806ff98da3c359f5
 
 fillNormFactor <- function(ID) {
   genome <- read.table(paste0(ID,"/genome"), quote="\"", comment.char="")
   ID <- read_table2(paste0(ID, "/", ID, "_factors.txt"))
   ID<-ID[,c(1:2)]
   ID$genome <- as.character(genome[[1]])
-<<<<<<< HEAD
   colnames(ID) <- c("sample_id", "norm_factor", "genome")
-=======
-  colnames(ID) <- c("sample_id", "normFactor", "genome")
->>>>>>> 88a810c224c4f51971cacb88806ff98da3c359f5
   ID$sample_id <- gsub(pattern = ".bedGraph", replacement = "", ID$sample_id)
   metadata <- merge(x = metadata, y = ID, by = 'sample_id')
 }
@@ -44,11 +36,7 @@ fillNormFactor <- function(ID) {
 final_metadata <- lapply(IDs, fillNormFactor)%>% bind_rows()
 
 
-<<<<<<< HEAD
 conn <- dbConnect(RSQLite::SQLite(), "/home/makis/elixir-RNAseq/data/metadata.sqlite")
-=======
-conn <- dbConnect(RSQLite::SQLite(), "/home/makis/elixir-RNAseq/config/metadata.sqlite")
->>>>>>> 88a810c224c4f51971cacb88806ff98da3c359f5
 dbWriteTable(conn, "metadata", final_metadata, overwrite = T)
 
 #dbGetQuery(conn, "SELECT * FROM metadata WHERE genome = 'mm10'")
